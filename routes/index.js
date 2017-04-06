a979136c369d35c21360cc95dc788d53fb147d18
mSeifried <seifried.mat@gmail.com>
2017-04-06 15:52:50 -0400

    Fix

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+
+// PayPal API Integration
+var payPalAccessToken = 'access_token$sandbox$b92mts6ng7s7xh3h$12ac80d3c73f14f567f8bb57b66bcfea';
+
+
+// End PayPal API Integration
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
