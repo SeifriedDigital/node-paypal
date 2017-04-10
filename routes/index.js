d8adeb1a3f1403501cb81a3637e55dcce54f0126
mSeifried <seifried.mat@gmail.com>
2017-04-10 18:11:55 -0400

    Broken

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,13 @@
-var express = require('express');
-var router = express.Router();
-var paypal = require('paypal-rest-sdk');
-var http = require('http');
+var express = require('express')
+var router = express.Router()
+var paypal = require('paypal-rest-sdk')
+var http = require('http')
 
 paypal.configure({
     'mode': 'sandbox', //sandbox or live
     'client_id': 'AVe3Wjq55GPvwXhIt0JkbVZKqR4b7TzmEOoqBBq0FnDHkcuXlFiXhWfD9IyV_Dcrm3msgq-nNE64rXQa',
     'client_secret': 'EIumHkaNXsOmY12YGfEoyzdvp1km_6YCjV1AcewSdPk_ZkIeVGqs3Hkou3M4yrQ7HBUCJSUynbpJicfA'
-});
+})
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -15,8 +15,37 @@ router.get('/', function(req, res, next) {
   res.render('index',
       {
         title: 'Express'
-      });
-});
+      })
+})
+
+router.get('/result', function(req, res, next) {
+    res.render('result', {
+        title: "Success",
+        state: "Success",
+        id: 123123
+    })
+})
+
+router.get('/result/:state/:id', function(req, res, next) {
+
+    // Get result state
+    var state = req.params.state
+
+    // Get sale ID from url
+    var id = req.params.id
+
+
+    if (state == 'success') {
+
+        console.log('Loading result view')
+
+        res.render('result', {
+            title: "Success",
+            state: state,
+            id: id
+        })
+    }
+})
 
 /* GET payment page. */
 router.post('/create-payment', function(req, res, next) {
@@ -27,7 +56,7 @@ router.post('/create-payment', function(req, res, next) {
             "payment_method": "paypal"
         },
         "redirect_urls": {
-            "return_url": "http://localhost:3000/execute-payment",
+            "return_url": "http://localhost:3000/result",
             "cancel_url": "http://localhost:3000/cancel"
         },
         "transactions": [{
@@ -46,26 +75,31 @@ router.post('/create-payment', function(req, res, next) {
             },
             "description": "This is the payment description."
         }]
-    };
+    }
 
 
     paypal.payment.create(create_payment_json, function (error, payment) {
         if (error) {
-            throw error;
+            throw error
         } else {
-            console.log("Create Payment Response");
-            console.log(payment);
+            console.log("Create Payment Response")
+            console.log(payment)
 
             res.json({
                 'paymentID': payment.id
             })
         }
-    });
+    })
+})
+
+router.get('/execute-payment', function(req, res, next) {
+    res.render('result')
 })
 
 router.post('/execute-payment', function(req, res, next) {
-    var paymentID = req.body.paymentID;
-    var payerID = req.body.payerID;
+    var paymentID = req.body.paymentID
+    var payerID = req.body.payerID
+
 
     var execute_payment_json = {
         "payer_id": payerID,
@@ -75,24 +109,22 @@ router.post('/execute-payment', function(req, res, next) {
                 "total": "1.00"
             }
         }]
-    };
-
+    }
 
     paypal.payment.execute(paymentID, execute_payment_json, function (error, payment) {
         if (error) {
-            console.log(error);
-            throw error;
+            console.log(error)
+            throw error
         } else {
-            console.log("Get Payment Response");
-            console.log(payment);
+            console.log("Get Payment Response")
+            console.log(JSON.stringify(payment))
+            var sale = payment.transactions[0].related_resources[0].sale
+            //
+            console.log(sale)
 
-            res.render('result',
-                {
-                    title: 'Success',
-                    msg: 'Transaction was confirmed!'
-                });
+            // res.redirect(304, 'http://localhost:3000/result/success/'+sale.id)
         }
-    });
-});
+    })
+})
 
-module.exports = router;
+module.exports = router
